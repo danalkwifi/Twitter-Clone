5741d8b9440228dfe31a47fa0bd837d2d7990ea6
dalkwifi <dalkwifi@uwo.ca>
2024-11-27 16:19:57 -0500

    Project setup

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,13 @@ import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
 app.use("/api/auth", authRoutes);
 
 app.listen(8000, () => {
-    console.log("Server is running on port 80000");
+    console.log(`Server is running on port ${PORT}`);
     connectMongoDB();
 });
