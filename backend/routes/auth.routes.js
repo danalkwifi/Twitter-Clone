455ff012a5630eda297cf10c40a6ebc34435a8b7
dalkwifi <dalkwifi@uwo.ca>
2024-11-29 12:28:09 -0500

    auth controllers added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,12 +1,13 @@
 import express from  'express';
-import { login, logout, signup } from '../controllers/auth.controller.js';
+import { getMe, login, logout, signup } from '../controllers/auth.controller.js';
+import { protectRotue } from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
+router.get('/me', protectRotue, getMe);
+
 router.post('/signup', signup);
-
 router.post('/login', login);
-
 router.post('/logout', logout);
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
@@ -9,9 +10,13 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
+app.use(express.json()); //to parse to req.body
+app.use(express.urlencoded({ extended: true })); //to parse form data
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectMongoDB();
 });
