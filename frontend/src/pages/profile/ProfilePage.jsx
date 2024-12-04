e1a48ceb6fc4c23c69676ece5b284ac44ef93dc8
dalkwifi <dalkwifi@uwo.ca>
2024-12-03 19:17:51 -0500

    profile page posts and user into fetched

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -1,5 +1,5 @@
-import { useRef, useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 import Posts from "../../components/common/Posts";
 import ProfileHeaderSkeleton from "../../components/skeletons/ProfileHeaderSkeleton";
@@ -11,6 +11,8 @@ import { FaArrowLeft } from "react-icons/fa6";
 import { IoCalendarOutline } from "react-icons/io5";
 import { FaLink } from "react-icons/fa";
 import { MdEdit } from "react-icons/md";
+import { useQuery } from "@tanstack/react-query";
+import { formatMemberSinceDate } from "../../utils/date";
 
 const ProfilePage = () => {
 	const [coverImg, setCoverImg] = useState(null);
@@ -20,20 +22,32 @@ const ProfilePage = () => {
 	const coverImgRef = useRef(null);
 	const profileImgRef = useRef(null);
 
-	const isLoading = false;
-	const isMyProfile = true;
+	const { username } = useParams();
 
-	const user = {
-		_id: "1",
-		fullName: "John Doe",
-		username: "johndoe",
-		profileImg: "/avatars/boy2.png",
-		coverImg: "/cover.png",
-		bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-		link: "https://youtube.com/@asaprogrammer_",
-		following: ["1", "2", "3"],
-		followers: ["1", "2", "3"],
-	};
+	const isMyProfile = true;
+	
+
+	const {
+		data,
+		isLoading,
+		refetch,
+		isRefetching,
+		isError
+	} = useQuery({
+			queryKey: ["userProfile"],
+			queryFn: async () =>{
+				try{
+					const res = await fetch(`/api/user/profile/${username}`);
+					const data = await res.json();
+					if(!res.ok){
+						throw new Error(data.error || "Something went wrong");
+				}
+				return data;
+				}catch(error){
+					throw new Error(error);
+				}
+			}
+	});
 
 	const handleImgChange = (e, state) => {
 		const file = e.target.files[0];
@@ -47,14 +61,38 @@ const ProfilePage = () => {
 		}
 	};
 
-	return (
+	
+	
+	useEffect(() => {
+		refetch();
+	}, [username, refetch]);
+
+	// Handle loading state
+	if (isLoading) {
+		return <p>Loading...</p>;
+	  }
+	
+	  // Handle error state
+	  if (isError) {
+		return <p>Error loading user profile.</p>;
+	  }
+	
+	  // Extract the username from the `data`
+	  const user = data?.user; // Safely access `user`
+	  if (!user) {
+		return <p>User not found</p>;
+	  }
+
+	  const memberSinceDate = formatMemberSinceDate(user?.createdAt);
+	
+	  return (
 		<>
 			<div className='flex-[4_4_0]  border-r border-gray-700 min-h-screen '>
 				{/* HEADER */}
-				{isLoading && <ProfileHeaderSkeleton />}
-				{!isLoading && !user && <p className='text-center text-lg mt-4'>User not found</p>}
+				{(isLoading || isRefetching) && <ProfileHeaderSkeleton />}
+				{!isLoading && !isRefetching && !user && <p className='text-center text-lg mt-4'>User not found</p>}
 				<div className='flex flex-col'>
-					{!isLoading && user && (
+					{!isLoading && !isRefetching && user && (
 						<>
 							<div className='flex gap-10 px-4 py-2 items-center'>
 								<Link to='/'>
@@ -84,14 +122,14 @@ const ProfilePage = () => {
 								<input
 									type='file'
 									hidden
-                                    accept="image/*"
+									accept='image/*'
 									ref={coverImgRef}
 									onChange={(e) => handleImgChange(e, "coverImg")}
 								/>
 								<input
 									type='file'
 									hidden
-                                    accept="image/*"
+									accept='image/*'
 									ref={profileImgRef}
 									onChange={(e) => handleImgChange(e, "profileImg")}
 								/>
@@ -155,7 +193,7 @@ const ProfilePage = () => {
 									)}
 									<div className='flex gap-2 items-center'>
 										<IoCalendarOutline className='w-4 h-4 text-slate-500' />
-										<span className='text-sm text-slate-500'>Joined July 2021</span>
+										<span className='text-sm text-slate-500'>{memberSinceDate}</span>
 									</div>
 								</div>
 								<div className='flex gap-2'>
@@ -192,10 +230,11 @@ const ProfilePage = () => {
 						</>
 					)}
 
-					<Posts />
+					<Posts feedType={feedType} username={username} userId={user?._id} />
 				</div>
 			</div>
 		</>
-	);
-};
+	  );
+	};
+	
 export default ProfilePage;
